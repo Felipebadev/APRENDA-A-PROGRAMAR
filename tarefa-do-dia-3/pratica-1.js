2f7f3778b5399d4360994dacdc2b34929cb29ed8
github.com/felipebadev <fbaraujo.fa@gmail.com>
2022-05-22 19:24:52 -0300

    adicionando as variaveis do passo 1

--- tarefa-do-dia-3/pratica-1.js
+++ tarefa-do-dia-3/pratica-1.js
@@ -3,3 +3,4 @@ let nome = "Felipe araujo"
 let Email = 'fbaraujo.fa@gmail.com'
 let profissao = 'tecnico suporte/operacional de telecom'
 let idade = '38'  
+let lugar = "sonho em conhecer a casa da minha mãe em minas gerais"
