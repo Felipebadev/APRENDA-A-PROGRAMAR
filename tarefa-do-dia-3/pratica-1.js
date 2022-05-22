dcc98a5ec016bf80967329f893e9077888c88b40
github.com/felipebadev <fbaraujo.fa@gmail.com>
2022-05-22 20:32:29 -0300

    adicionando as variaveis do passo 1

--- tarefa-do-dia-3/pratica-1.js
+++ tarefa-do-dia-3/pratica-1.js
@@ -6,3 +6,13 @@ let idade = '38'
 let lugar = "sonho em conhecer a casa da minha mãe em minas gerais" 
 
 console.log(nome, email, profissao, idade)
+
+console.log(para que os valores de cada uma das variaveis se encaixe nessa frase que deve 
+ser exibida na tela: 
+Ola, <valor da variavel que tem o seu nome>! 
+Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar
+as compras das passagens aereas para <valor da variavel que armazena o lugar que voce sonha conhecer>.
+ Confirma pra gente alguns dados? A sua idade e  <valor da variavel que armazena sua idade>, 
+ voce e <valor da variavel que armazena sua profissao> e o seu email e <valor da variavel que armazena 
+ seu email>? 
+ Ficamos no aguardo. Muito obrigado e boa viagem! :D)
