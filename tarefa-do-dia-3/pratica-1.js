3a52329d5cc243fd3daa58f9b7d57b4bee7fbbdc
github.com/felipebadev <fbaraujo.fa@gmail.com>
2022-05-24 22:14:13 -0300

    adicionando a pratica-1.js

--- tarefa-do-dia-3/pratica-1.js
+++ tarefa-do-dia-3/pratica-1.js
@@ -1,8 +1,15 @@
-var = {
-   let nome = "felipe" 
-   let email = "fbaraujo.fa@gmail.com"
-   let profissao = "agente de coleta de dados"
-   let idade = "38"
-   let lugar_que_quer_conhecer = "minas gerais" 
-
+let pessoa = {
+   nome: "felipe", 
+   email: 'fbaraujo.fa@gmail.com',
+   profissao: "agente de coleta de dados",
+   idade: "38",
+   lugar_que_quer_conhecer: "minas gerais", 
 }
+
+console.log(pessoa.nome)
+console.log(pessoa.email)
+console.log(pessoa.profissao)
+console.log(pessoa.idade)
+console.log(pessoa.lugar_que_quer_conhecer)
+
+
